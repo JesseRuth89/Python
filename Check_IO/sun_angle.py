f4725eb6d91613a2e8b98adee2cc67927747b38c
Radwatch <74883877+Radwatch@users.noreply.github.com>
2021-05-18 22:14:33 -0500

    Create sun_angle.py

---
+++ Check_IO/sun_angle.py
@@ -0,0 +1,18 @@
+def sun_angle(time):
+    # Splitting the string into a list
+    time = time.split(":")
+    # Converting the base 60 min to a float
+    time = float(time[0]) + float(time[1]) * (1/60)
+    # only return when the sun is betwen 6am and 6pm
+    if time < 6.0 or time > 18.00:
+        return "I don't see the sun!"
+    else:
+        # Math... 360 degrees total, with 24 hours total, minus the 90 degree offset
+        angle = time / 24 * 360 - 90
+        return round(angle, 2)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sun_angle("14:23"))
+
