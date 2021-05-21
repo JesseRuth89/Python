b6fd4c648d11ab5547be1af75fd18bcf5e6c5296
Radwatch <74883877+Radwatch@users.noreply.github.com>
2021-05-20 20:35:43 -0500

    Update leap_year.py

--- hackerrank/leap_year.py
+++ hackerrank/leap_year.py
@@ -1,15 +1,21 @@
+"""
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+"""
+import calendar
 def is_leap(year) -> bool:
+    """ This is 'stupid' be smart with returns
     if year % 4 == 0:
         leap = True
         if year % 100 == 0:
-            leap = True
-        elif year % 400 == 0:
-            leap = True
-    else:
-        leap = False
-    return leap
+            leap = False
+            if year % 400 == 0:
+                leap = True
+    """
+    # import the calendar dummy
+    # return calendar.isleap(year)
+    # single line
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
-
-assert is_leap(2000) == True
-assert is_leap(2400) == True
-assert is_leap(2500) == False
+print(is_leap(2500))
