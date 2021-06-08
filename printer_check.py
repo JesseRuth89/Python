2db35c3acb86895e6f9fd338eadafdbf37799228
JR019593 <jesse.ruth@cerner.com>
2021-06-08 15:23:01 -0500

    Update printer_check.py

--- printer_check.py
+++ printer_check.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
-import os, shlex, subprocess
+import subprocess
+import socket
+import time
+import concurrent.futures
 
-def gather_printers():
-        output = subprocess.Popen(["lpstat", "-v"],
-                                  universal_newlines=True,
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.STDOUT)
-        out, err = output.communicate()
-        printers = []
-        for line in out.splitlines():
-                name = line.split()[3].split(":")[1].strip("//")
-                try:
-                        port = line.split()[3].split(":")[2]
-                except IndexError:
-                        # defaulting to 9100
-                        # port is not defined for the socket
-                        print(name)
-                        port = 9100
-                printers.append({"name": name, "port": int(port)})
-        return printers
-print(gather_printers())
+
+def gather_printers() -> list:
+    output = subprocess.Popen(["lpstat", "-v"],
+                              universal_newlines=True,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT)
+    # execute lpstat
+    out, err = output.communicate()
+    printers = []
+    # for each line gather name and port
+    for line in out.splitlines():
+        try:
+            name = line.split()[3].split(":")[1].strip("//")
+            printers.append(name)
+        except:
+            print(f'****Check into {line}****')
+    return printers
+
+
+def port_check(address):
+    s = socket.socket()
+    s.settimeout(0.2)
+    try:
+        s.connect((address, 9100))
+        # return f'Connected to {address} on port 9100'
+    except OSError:
+        return f'Failed to connect to {address}'
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(port_check, printer) for printer in gather_printers()]
+        for result in concurrent.futures.as_completed(results):
+            print(result.result())
+
+    end = time.perf_counter()
+    print(f'Finished in: {round(end - start, 2)} second(s)')
