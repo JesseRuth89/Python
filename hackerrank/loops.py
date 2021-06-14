575a646e2b4818e881aa51a4e9cc3e06cf5a487f
JR019593 <jesse.ruth@cerner.com>
2021-06-14 11:20:48 -0500

    updates

--- hackerrank/loops.py
+++ hackerrank/loops.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    n = int(5)
+    n = 5
     for x in range(n):
         # power operator is **
         print(x ** 2)
--- printer_check.py
+++ printer_check.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import subprocess
+import logging
 import socket
 import time
 import concurrent.futures
+import os
 
 
 def gather_printers() -> list:
@@ -15,31 +17,50 @@ def gather_printers() -> list:
     printers = []
     # for each line gather name and port
     for line in out.splitlines():
+        logging.debug(f'Printer being parsed:{line}')
         try:
             name = line.split()[3].split(":")[1].strip("//")
+            logging.debug(f'Printer being appended:{name}')
             printers.append(name)
         except:
-            print(f'****Check into {line}****')
+            logging.warning(f'Unable to parse {line}')
     return printers
 
 
-def port_check(address):
+def port_check(address, port):
     s = socket.socket()
-    s.settimeout(0.2)
+    s.settimeout(.5)
     try:
-        s.connect((address, 9100))
-        # return f'Connected to {address} on port 9100'
+        s.connect((address, port))
+        logging.info(f'Connected to {address} over {port}')
     except OSError:
-        return f'Failed to connect to {address}'
+        logging.warning(f'Failed to connect to {address} over {port}')
 
 
+def main():
+    # creating log in current working dir
+    logging.basicConfig(
+        filename='printer_check.log',
+        # new log file each run
+        filemode='w',
+        level=logging.DEBUG
+    )
+    logging.info('Printer check started')
+    print('Gathering system printer(s)...')
+    # printers = gather_printers()
+    port = 22
+    printers = ['shsohapp1', 'shsohapp2', 'shsohapp3', 'baycflapp1', 'baycfldb4']
+    print(f'{len(printers)} printer(s) defined on this system')
+
+    # multi-threading port_check via executor
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        [executor.submit(port_check, printer, port) for printer in printers]
+
+    logging.info('Printer check ended')
 if __name__ == '__main__':
     start = time.perf_counter()
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = [executor.submit(port_check, printer) for printer in gather_printers()]
-        for result in concurrent.futures.as_completed(results):
-            print(result.result())
-
+    main()
     end = time.perf_counter()
     print(f'Finished in: {round(end - start, 2)} second(s)')
+    # debug this message
+    logging.debug(f'Finished in: {round(end - start, 2)} second(s)')
