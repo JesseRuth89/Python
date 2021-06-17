8613ef21d7ea0b4ef14fff5b39f91970a41e57b8
JR019593 <jesse.ruth@cerner.com>
2021-06-17 13:47:12 -0500

    vertica example

--- api_tester.py
+++ api_tester.py
@@ -1,3 +1,24 @@
+from bs4 import BeautifulSoup
+import lxml
 import requests
+import time
 
-print(requests.get('http://configcert.cernerasp.com/configuration-1.0/'))
+
+start = time.perf_counter()
+# POST GET on configuration uri
+r = requests.get('http://config.cernerasp.com/configuration-1.0/p126.univ_nm.cernerasp.com/urn:cerner:config:xr:network-file-share-1.0')
+
+soup = BeautifulSoup(r.text, 'lxml-xml')
+shares = soup.find_all('share')
+
+for share in shares:
+    name = share.get('display')
+    path = share.get('path')
+    print(f'Share:{name} Path:{path}')
+
+# dsn_tns = cx_Oracle.makedsn('univnmdb1.cernerasp.com', '1521', service_name='s77_p126.world')
+# conn = cx_Oracle.connect(user='v500', password='Un1v500', dsn=dsn_tns)
+
+
+end = time.perf_counter()
+print(f'Finished in: {round(end - start, 2)} second(s)')
---
+++ vertica.py
@@ -0,0 +1,46 @@
+# pip install vertica-python
+import vertica_python
+import time
+
+# Vertica example
+
+conn_info = {'host': 'CERNOCRSVERTDB-QUERY.CERNERASP.com',
+             'port': 5433,
+             'user': 'jr019593',
+             'password': 'Jes496212?',
+             'database': 'Cerner',
+             # default throw error on invalid UTF-8 results
+             'unicode_error': 'strict',
+             'autocommit': True,
+             # using server-side prepared statements is disabled by default
+             'use_prepared_statements': False,
+             # 5 seconds timeout for a socket operation (Establishing a TCP connection or read/write operation)
+             # this might need to be adjusted, it will randomly timeout
+             'connection_timeout': 5}
+
+# using with for auto connection closing after usage
+query = "select distinct client,domain,instancehost, instancename " \
+        "from " \
+        "olyprd.oracle_instance " \
+        "where instancenum = 1 " \
+        "and domain in(select DISTINCT domain " \
+        "from OLYPRD.CLIENT_DOMAIN_NODE " \
+        "where production_ind = 1 " \
+        "and millennium_ind = 1) " \
+        "and collectdatetime > sysdate -1 " \
+        "order by client"
+start = time.perf_counter()
+print(f'Running query')
+with vertica_python.connect(**conn_info) as connection:
+    cur = connection.cursor()
+    cur.execute(query)
+    for results in cur.fetchall():
+        print(results)
+end = time.perf_counter()
+print(f'Query executed in:{round(end - start, 2)} second(s)')
+
+
+
+
+
+
