00227964512283aef7a4ed3c386c736ab4510eb4
Jesse Ruth <74883877+JesseRuth89@users.noreply.github.com>
2022-02-18 16:16:43 -0600

    Update speedtest.py

--- speedtester/speedtest.py
+++ speedtester/speedtest.py
@@ -1,79 +1,48 @@
-import subprocess
+import psycopg2
+import dbcreds
 import os
-import sqlite3
-from datetime import datetime
-import json
 import time
+import subprocess
+import json
+from datetime import datetime, timezone
 
 
-def define_database(db_name):
-    try:
-        con = sqlite3.connect(db_name,
-                              detect_types=sqlite3.PARSE_DECLTYPES |
-                                           sqlite3.PARSE_COLNAMES)
-        cur = con.cursor()
-        print(f'Checking for speedtest table in {db_name}')
-        table_result = cur.execute("""SELECT EXISTS (
-                                    SELECT name FROM sqlite_schema
-                                     WHERE type = 'table' 
-                                     AND name = 'speedtest')""").fetchone()
-        if table_result[0] == 0:
-            print(f'Creating table speedtest')
-            cur.execute("""CREATE TABLE speedtest (
-                            collectdatetime timestamp UNIQUE
+def define_db():
+    creds = dbcreds.Credentials()
+    directory = os.path.dirname(os.path.realpath(__file__))
+    key = 'db.key'
+    if not os.path.exists(os.path.join(directory, key)):
+        print(f'****No DB credentials found****')
+        creds.username = input('Enter your DB password:')
+        creds.password = input('Enter your DB password:')
+        creds.create_cred()
+    else:
+        print(f'DB credentials found\n'
+              f'username: {creds.getuser()}\n'
+              f'password: *********')
+    host = 'speedtestdb.postgres.database.azure.com'
+    dbname = 'postgres'
+    user = creds.getuser()
+    password = creds.getpass()
+    sslmode = "require"
+    # Construct connection string
+    conn_string = f'host={host} user={user} dbname={dbname} password={password} sslmode={sslmode}'
+    conn = psycopg2.connect(conn_string)
+    print(f'Connected to {host}')
+    with conn:
+        with conn.cursor() as cur:
+            # define database for our usage
+            cur.execute("""CREATE TABLE IF NOT EXISTS speedtest (
+                            collectdatetime timestamptz UNIQUE
                             ,download integer
                             ,upload integer
-                            ,latency integer
-                            ,jitter integer
+                            ,latency decimal
+                            ,jitter decimal
                             ,packetLoss integer
                             ,result_url text
-                            ,server_id text
-                            )""")
-            con.commit()
-        else:
-            print('Table already exists')
-    except sqlite3.Error as error:
-        print(f'SQL connection error: {error}')
-    finally:
-        if con:
-            con.close()
-
-
-def update_database(insert: dict, db_name: str):
-    r = insert
-    # uses output of speedtest json for sqlite injection
-    formatted_insert = {'collectdatetime': datetime.now()
-        , 'download': r['download']['bandwidth']
-        , 'upload': r['upload']['bandwidth']
-        , 'latency': r['ping']['latency']
-        , 'jitter': r['ping']['jitter']
-        , 'packetLoss': r['packetLoss']
-        , 'result_url': r['result']['url']
-        , 'server_id': r['server']['id']
-                        }
-    try:
-        con = sqlite3.connect(db_name,
-                              detect_types=sqlite3.PARSE_DECLTYPES |
-                                           sqlite3.PARSE_COLNAMES)
-        cur = con.cursor()
-        print('Inserting results into the db')
-        cur.execute("""INSERT into speedtest 
-                        VALUES(
-                        :collectdatetime 
-                        ,:download
-                        ,:upload
-                        ,:latency
-                        ,:jitter
-                        ,:packetLoss
-                        ,:result_url
-                        ,:server_id
-                        )""", formatted_insert)
-        con.commit()
-    except sqlite3.Error as error:
-        print(f'Error inserting data into database: {error}')
-    finally:
-        if con:
-            con.close()
+                            ,server_id text);
+                        """)
+    return conn
 
 
 def run_speedtest():
@@ -86,23 +55,43 @@ def run_speedtest():
         results = json.loads(subprocess.check_output([speedtest, '--f', 'json'], universal_newlines=True))
         end = time.perf_counter()
         print(f'Finished in {round(end - start, 2)} second(s)')
+
         # print(results)
         return results
     except subprocess.CalledProcessError as error:
         print(f'Error running speedtest.exe: {error}')
 
 
-def main():
-    # defining our database location
-    db_location = os.path.dirname(os.path.realpath(__file__))
-    db_name = 'speedtest.db'
-    db_full = os.path.join(db_location, db_name)
-    define_database(db_full)
+def update_database(insert: dict, connection):
+    r = insert
+    con = connection
+    # uses output of speedtest json for sqlite injection
+    formatted_insert = {'collectdatetime': datetime.now(timezone.utc)
+        , 'download': r['download']['bandwidth']
+        , 'upload': r['upload']['bandwidth']
+        , 'latency': r['ping']['latency']
+        , 'jitter': r['ping']['jitter']
+        , 'packetLoss': r['packetLoss']
+        , 'result_url': r['result']['url']
+        , 'server_id': r['server']['id']
+                        }
+    with conn:
+        with conn.cursor() as cur:
+            print('Inserting results into the db')
+            cur.execute("""INSERT into speedtest
+                                VALUES(
+                                'now'
+                                ,%(download)s
+                                ,%(upload)s
+                                ,%(latency)s
+                                ,%(jitter)s
+                                ,%(packetLoss)s
+                                ,%(result_url)s
+                                ,%(server_id)s 
+                                )""", formatted_insert)
 
-    # runs speedtest and appends values into db
-    update_database(run_speedtest(), db_full)
-    print('Completed!')
 
-
-if __name__ == '__main__':
-    main()
+conn = define_db()
+speedtest_results = run_speedtest()
+update_database(speedtest_results, conn)
+conn.close()
